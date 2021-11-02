f993480774e4e04b442a7178361d51565f5a9359
naem1023 <relilau00@gmail.com>
2021-11-02 11:34:16 +0900

    Add

--- sort/pg-30-17686.py
+++ sort/pg-30-17686.py
@@ -5,7 +5,7 @@ def solution(files):
     files_split = [re.split(r'(\d+)', file) for file in files]
     files_split.sort(key = lambda x : (x[0].lower(), int(x[1])))
     
-    for i in array:
+    for i in files_split:
         answer.append(''.join(i)) 
     
     return answer
