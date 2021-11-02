3a532fc3b70b76d5229d110d3ea4fd0ab65fee39
naem1023 <relilau00@gmail.com>
2021-11-02 11:30:13 +0900

    Add

--- pg-30-12981.py
+++ pg-30-12981.py
@@ -3,7 +3,7 @@ def solution(n, words):
     stack = [words[0]]
     for i in range(1, len(words)):
         # stack의 가장 윗 단어의 마지막 글자와 i번째 단어의 시작이 같고
-        # i번재 단어가 스택에 없다면
+        # i번째 단어가 스택에 없다면
         if stack[-1][-1] == words[i][0] \
             and words[i] not in stack:
             stack.append(words[i])
---
+++ sort/pg-30-17686.py
@@ -0,0 +1,11 @@
+import re
+
+def solution(files):
+    answer = []
+    files_split = [re.split(r'(\d+)', file) for file in files]
+    files_split.sort(key = lambda x : (x[0].lower(), int(x[1])))
+    
+    for i in array:
+        answer.append(''.join(i)) 
+    
+    return answer
